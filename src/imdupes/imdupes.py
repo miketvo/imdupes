1c054636a33b44b4d74297dc5512b42c909c4975
miketvo <ikxplicitie@gmail.com>
2023-06-05 06:39:35 +0700

    Added warning message "Warning: Deleted files are not recoverable, proceed with caution" in argparse description

--- src/imdupes/imdupes.py
+++ src/imdupes/imdupes.py
@@ -43,7 +43,8 @@ if __name__ == '__main__':
             usage='imdupes {detect,clean} [OPTIONS] DIRECTORY',
             description="Quickly detects and removes identical images. Has two modes:\n"
                         "\t- 'detect' console prints the detected identical image paths/filenames\n"
-                        "\t- 'clean' removes the detected identical images, keeping only the first copy",
+                        "\t- 'clean' removes the detected identical images, keeping only the first copy\n"
+                        "Warning: Deleted files are not recoverable, proceed with caution",
             epilog='Note: This program ignores any non-image file in the target directory\n'
                    '\n'
                    '*: Smaller hash sizes are better at detecting visually similar images, while larger hash sizes are'
