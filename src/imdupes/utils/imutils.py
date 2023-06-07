2e994a9f6ed27e7b0fe69cd83278dbfe5d69ae47
miketvo <ikxplicitie@gmail.com>
2023-06-07 12:49:43 +0700

    Implemented dupfile.py

--- src/imdupes/utils/imutils.py
+++ src/imdupes/utils/imutils.py
@@ -2,6 +2,6 @@ import PIL.Image
 
 
 class ImageFileWrapper:
-    def __init__(self, image: PIL.Image, path: str):
+    def __init__(self, image: PIL.Image = None, path: str = None):
         self.image = image
         self.path = path
