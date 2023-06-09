b0a6305c52cd94a7a1f3433adeacdf978e81e479
miketvo <ikxplicitie@gmail.com>
2023-06-09 15:56:02 +0700

    Moved scraped test data to its own folder

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,7 @@
 import os
 
 URL_DATA_SRC = 'https://pokemondb.net/pokedex/national'
-DATA_DIR = os.path.abspath('data/')
+DATA_DIR = os.path.abspath('data/images/scraped/')
 DATA_DUPLICATE_PERCENTAGE = 0.2  # Percentage of images to be duplicated in test data
 DATA_MAX_DUPLICATES = 3          # Maximum number of duplicated images in test data
 DATA_RANDOM_SEED = 42            # Random seed for reproducibility of test data duplication process
