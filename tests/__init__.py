ee0ba381bda1069a4615eaf3af4e91b1a6edb359
miketvo <ikxplicitie@gmail.com>
2023-06-09 21:21:35 +0700

    Better naming for tests.DATA_DIR --> DIR_DATA_SCRAPED

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,8 @@
 import os
 
 URL_DATA_SRC = 'https://pokemondb.net/pokedex/national'
-DATA_DIR = os.path.abspath('data/images/scraped/')
+DIR_DATA = os.path.abspath('data/images/')
+DIR_DATA_SCRAPED = os.path.abspath(f'{DIR_DATA}scraped/')
 DATA_DUPLICATE_PERCENTAGE = 0.2  # Percentage of images to be duplicated in test data
 DATA_MAX_DUPLICATES = 3          # Maximum number of duplicated images in test data
 DATA_RANDOM_SEED = 42            # Random seed for reproducibility of test data duplication process
