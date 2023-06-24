d5e996dbdc76885d27bcd274e32a0589b5afe69b
miketvo <ikxplicitie@gmail.com>
2023-06-24 12:20:17 +0700

    Revert last change because it wasn't effective

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -1,7 +1,7 @@
 import unittest
 import os
 
-from imdupes.detect_dup_images import detect_dup_images
+from detect_dup_images import detect_dup_images
 from utils.globs import HashingMethod
 from tests import DIR_DATA_SCRAPED
 
