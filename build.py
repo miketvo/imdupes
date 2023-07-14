ea4d988f39eee961dfe65910891c883930093fc5
miketvo <ikxplicitie@gmail.com>
2023-07-14 10:20:19 +0700

    Added support for flexible build.py for both Windows & Linux using specfiles; Added Windows' supporting binaries; Bumped version number

--- build.py
+++ build.py
@@ -1,15 +1,19 @@
+from sys import platform
 import PyInstaller.__main__
 
 
-def build():
+def build(specfile: str = None):
     PyInstaller.__main__.run([
         'src/imdupes/imdupes.py',
-        '--onefile',
-        '--icon', 'icon.png',
-        '--console',
-        '--clean'
+        '--clean',
+        f'{specfile}'
     ])
 
 
 if __name__ == '__main__':
-    build()
+    if platform.startswith('win'):
+        build('windows-build.spec')
+    elif platform.startswith('linux'):
+        build('linux-build.spec')
+    else:
+        print(f'Platform "{platform}" not supported!')
