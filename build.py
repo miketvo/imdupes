a97791d6edc43d19c24e43bc701658fed7b6c1d0
miketvo <ikxplicitie@gmail.com>
2023-07-14 10:35:32 +0700

    Bumped version number; Fixed build.py

--- build.py
+++ build.py
@@ -4,16 +4,18 @@ import PyInstaller.__main__
 
 def build(specfile: str = None):
     PyInstaller.__main__.run([
-        'src/imdupes/imdupes.py',
         '--clean',
-        f'{specfile}'
+        f'{specfile}',
     ])
 
 
 if __name__ == '__main__':
+    print(f'Detected platform: "{platform}".', end='')
     if platform.startswith('win'):
+        print('\n')
         build('windows-build.spec')
     elif platform.startswith('linux'):
+        print('\n')
         build('linux-build.spec')
     else:
-        print(f'Platform "{platform}" not supported!')
+        print(f'Platform not supported!')
