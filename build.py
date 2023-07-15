9618ae466e7cc4b50b0f2a68ff8be221a813d393
miketvo <ikxplicitie@gmail.com>
2023-07-15 23:57:31 +0700

    Updated build.py to also output a checksum for the archive file; build.yml then uploads this checksum to GitHub artefact; Renamed test file

--- build.py
+++ build.py
@@ -1,13 +1,22 @@
 # noinspection PyProtectedMember
 from imdupes._version import __version__
+import os
+import shutil
 from sys import platform
 import sys
 import zipfile
 import tarfile
+import hashlib
 import PyInstaller.__main__
 
 
 def build(specfile: str = None) -> None:
+    if os.path.exists('build'):
+        shutil.rmtree('build')
+
+    if os.path.exists('dist'):
+        shutil.rmtree('dist')
+
     PyInstaller.__main__.run([
         '--clean',
         f'{specfile}',
@@ -15,13 +24,25 @@ def build(specfile: str = None) -> None:
 
 
 def create_zip_archive(os_name: str) -> None:
-    with zipfile.ZipFile(f'dist/imdupes-{__version__}-{os_name}.zip', 'w') as zipf:
-        zipf.write('dist/imdupes.exe')
+    with zipfile.ZipFile(f'dist/imdupes-v{__version__}-{os_name}.zip', 'w') as zipf:
+        zipf.write('dist/imdupes.exe', arcname='imdupes.exe')
 
 
 def create_tar_archive(os_name: str) -> None:
-    with tarfile.open(f'dist/imdupes-{__version__}-{os_name}.tar', 'w') as tar:
-        tar.add('dist/imdupes')
+    with tarfile.open(f'dist/imdupes-v{__version__}-{os_name}.tar', 'w') as tar:
+        tar.add('dist/imdupes', arcname='imdupes')
+
+
+def create_checksum_file(os_name: str, file_extension: str) -> None:
+    file_path = f'dist/imdupes-v{__version__}-{os_name}.{file_extension}'
+    checksum = hashlib.sha256()
+    with open(file_path, 'rb') as file:
+        for chunk in iter(lambda: file.read(4096), b''):
+            checksum.update(chunk)
+    checksum_value = checksum.hexdigest()
+    checksum_file_path = f'dist/imdupes-v{__version__}-{os_name}.{file_extension}.sha256'
+    with open(checksum_file_path, 'w') as checksum_file:
+        checksum_file.write(f'{checksum_value}  imdupes-v{__version__}-{os_name}.{file_extension}')
 
 
 if __name__ == '__main__':
@@ -30,14 +51,17 @@ if __name__ == '__main__':
         print('\n')
         build('windows-build.spec')
         create_zip_archive('windows')
+        create_checksum_file('windows', 'zip')
     elif platform.startswith('linux'):
         print('\n')
         build('unix-build.spec')
         create_tar_archive('linux')
+        create_checksum_file('linux', 'tar')
     elif platform.startswith('darwin'):
         print('\n')
         build('unix-build.spec')
         create_tar_archive('macos')
+        create_checksum_file('macos', 'tar')
     else:
         print(f'Platform not supported!', file=sys.stderr)
         exit(-1)
