7d649f3ec6a129618b5d71dd4adf7922f59bc777
miketvo <ikxplicitie@gmail.com>
2023-07-16 00:51:21 +0700

    More verbose build.py

--- build.py
+++ build.py
@@ -26,11 +26,13 @@ def build(specfile: str = None) -> None:
 def create_zip_archive(os_name: str) -> None:
     with zipfile.ZipFile(f'dist/imdupes-v{__version__}-{os_name}.zip', 'w') as zipf:
         zipf.write('dist/imdupes.exe', arcname='imdupes.exe')
+    print(f'Executable archived in "dist/imdupes-v{__version__}-{os_name}.zip"')
 
 
 def create_tar_archive(os_name: str) -> None:
     with tarfile.open(f'dist/imdupes-v{__version__}-{os_name}.tar', 'w') as tar:
         tar.add('dist/imdupes', arcname='imdupes')
+    print(f'Executable archived in "dist/imdupes-v{__version__}-{os_name}.tar"')
 
 
 def create_checksum_file(os_name: str, file_extension: str) -> None:
@@ -40,9 +42,11 @@ def create_checksum_file(os_name: str, file_extension: str) -> None:
         for chunk in iter(lambda: file.read(4096), b''):
             checksum.update(chunk)
     checksum_value = checksum.hexdigest()
+    print(f'Archive checksum: {checksum_value}')
     checksum_file_path = f'dist/imdupes-v{__version__}-{os_name}.{file_extension}.sha256'
     with open(checksum_file_path, 'w') as checksum_file:
         checksum_file.write(f'{checksum_value}  imdupes-v{__version__}-{os_name}.{file_extension}')
+    print(f'Archive checksum saved in "dist/imdupes-v{__version__}-{os_name}.{file_extension}.sha256"')
 
 
 if __name__ == '__main__':
@@ -53,18 +57,21 @@ if __name__ == '__main__':
     if platform.startswith('win'):
         print('\n')
         build('windows-build.spec')
+        print('\n')
         create_zip_archive('windows')
         create_checksum_file('windows', 'zip')
     elif platform.startswith('linux'):
         print('\n')
         build('unix-build.spec')
+        print('\n')
         create_tar_archive('linux')
         create_checksum_file('linux', 'tar')
     elif platform.startswith('darwin'):
         print('\n')
         build('unix-build.spec')
+        print('\n')
         create_tar_archive('macos')
         create_checksum_file('macos', 'tar')
     else:
-        print(f'Platform not supported!', file=sys.stderr)
+        print(f' Platform not supported!', file=sys.stderr)
         exit(-1)
