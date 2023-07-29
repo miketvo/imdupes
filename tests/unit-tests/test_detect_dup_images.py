883ece496c1032fcf0de451014c360d79b4f9ce0
miketvo <ikxplicitie@gmail.com>
2023-07-29 11:09:58 +0700

    Refactored test_detect_dup_images.py

--- tests/unit-tests/test_detect_dup_images.py
+++ tests/unit-tests/test_detect_dup_images.py
@@ -4,8 +4,8 @@ import os
 from os.path import dirname
 
 
-sys.path.append(os.path.join(dirname(__file__) + '/..', 'src/imdupes'))
-sys.path.append(os.path.join(dirname(__file__) + '/..', 'tests'))
+sys.path.append(os.path.join(dirname(__file__) + '/../..', 'src/imdupes'))
+sys.path.append(os.path.join(dirname(__file__) + '/../..', 'tests'))
 from detect_dup_images import detect_dup_images
 from utils.globs import HashingMethod
 from tests import DIR_DATA_SCRAPED
@@ -25,19 +25,22 @@ def get_test_dups() -> list[str]:
 
 class Detect(unittest.TestCase):
     def test(self):
+        # Arrange
         test_dups = get_test_dups()
 
+        # Act
         detect_dups_dict = detect_dup_images(
             [os.path.join(DIR_DATA_SCRAPED, img) for img in os.listdir(DIR_DATA_SCRAPED)],
             method=HashingMethod.RGBA,
             root_dir=DIR_DATA_SCRAPED,
             verbose=True
         )
+
+        # Assert
         detect_dups = []
         for dup_imgs in detect_dups_dict.values():
             for dup_img in dup_imgs:
                 detect_dups.append(os.path.basename(dup_img.path))
-
         self.assertCountEqual(test_dups, detect_dups)  # add assertion here
 
 
